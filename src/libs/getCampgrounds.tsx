b4906890262c978342c916dff373effa057263d2
MasterTintin <6733031821@student.chula.ac.th>
2025-03-24 13:59:08 +0700

    Add Places Pictures

--- src/libs/getCampgrounds.tsx
+++ src/libs/getCampgrounds.tsx
@@ -1,6 +1,6 @@
 export default async function getCampgrounds() {
     await new Promise((resolve) => setTimeout(resolve, 300));
-    const response = await fetch("http://localhost:5000/api/v1/venues")
+    const response = await fetch("http://localhost:5000/api/v1/campgrounds")
     if(!response.ok) {
         throw new Error("Failed to fetch venues")
     }
